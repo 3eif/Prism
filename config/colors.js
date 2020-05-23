86b2f28cf983d48052898b6b93cd5346c2ffd7b3
Tetra <seifaziz10@gmail.com>
2020-05-23 07:41:03 -0700

    Create colors.js

---
+++ config/colors.js
@@ -0,0 +1,10 @@
+module.exports = {
+    'main': '#a5a1f8',
+    'online': '#7BCBA7',
+    'offline': '#9EA5AF',
+    'youtube': '#a5a1f8',
+    'mixer': '#1FBAED',
+    'soundcloud': '#FD8B1B',
+    'twitch': '#6442A4',
+    'bandcamp': '#619AA9',
+};
